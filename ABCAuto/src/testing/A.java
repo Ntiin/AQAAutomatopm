05c88ef95c75efca74bcb0cb81e7790dae8dc520
TestingWorld <TestingWorld@TestingWorld-PC>
2016-08-13 11:44:27 +0530

    werewrew

---
+++ ABCAuto/src/testing/A.java
@@ -0,0 +1,33 @@
+package testing;
+
+import java.io.IOException;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+	 
+		int i=10;
+		int j=0;
+		int c=i/j;
+		System.out.println(c);
+		System.out.println("Program end");
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	 
+	 
+	 
+	 
+	 
+		
+	}
+	
+}
